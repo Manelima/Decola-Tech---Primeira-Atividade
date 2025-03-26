1437fd01f865601338bf09d4d08ee006c9eb3842
Emanuel <emanuellima4321@gmail.com>
2025-03-26 18:56:45 -0300

    "Primeira Atividade: Dio"

---
+++ src/main/java/dio/decolatech/decolatech_2025/Decolatech2025Application.java
@@ -0,0 +1,15 @@
+package dio.decolatech.decolatech_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Decolatech2025Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Decolatech2025Application.class, args);
+
+	}
+
+}

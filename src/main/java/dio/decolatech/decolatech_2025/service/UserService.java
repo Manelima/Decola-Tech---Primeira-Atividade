c5ac8b9fcc5337cbd8fe133b600eb51c9a099025
Emanuel <emanuellima4321@gmail.com>
2025-03-29 01:17:13 -0300

    "Implementado: Pré-nuvem"

--- src/main/java/dio/decolatech/decolatech_2025/service/UserService.java
+++ src/main/java/dio/decolatech/decolatech_2025/service/UserService.java
@@ -1,8 +1,10 @@
 package dio.decolatech.decolatech_2025.service;
 
+import dio.decolatech.decolatech_2025.domain.model.User;
 
 public interface UserService {
-    boolean existsByAccountNumber(String number);
 
-    boolean existsByCardNumber(String number);
+    User findById(Long id);
+
+    User create(User userToCreate);
 }
--- src/main/java/dio/decolatech/decolatech_2025/service/impl/UserServiceImpl.java
+++ src/main/java/dio/decolatech/decolatech_2025/service/impl/UserServiceImpl.java
@@ -6,27 +6,25 @@ import dio.decolatech.decolatech_2025.service.UserService;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
-
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
-    public UserRepository(UserRepository userRepository){
-
+    public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
     @Override
-    public User findById(Long id){
+    public User findById(Long id) {
         return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
     }
 
     @Override
-    public User create(User userToCreate){
-        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())){
-            throw new IllegalAccessException("This Account already exists.");
+    public User create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This Account number already exists.");
         }
         return userRepository.save(userToCreate);
-}
-
+    }
 }
